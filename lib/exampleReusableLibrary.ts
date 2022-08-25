52961bc5ef2d7ea5590956e9116c75fcf0cff652
Terry Tan <tanjunweiterry@gmail.com>
2022-08-26 00:03:34 +0800

    typescript setup

--- lib/exampleReusableLibrary.ts
+++ lib/exampleReusableLibrary.ts
@@ -4,3 +4,7 @@
 // 1. reusable libraries i.e. functions that you might wanna use repeatedly for some computation e.g. subtractDate(biggerDate, smallerDate)
 // 2. custom hooks
 // 3. helper functions
+
+export const deleteMeLater = () => {
+  // stub
+};
---
+++ lib/hooks.ts
@@ -0,0 +1,3 @@
+export const hooks = () => {
+  // stub
+};
