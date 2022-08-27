4e7f9a02f092db4f138bb0568447f4c577035748
Terry Tan <tanjunweiterry@gmail.com>
2022-08-27 12:38:24 +0800

    tailwind css setup

--- components/exampleComponent.tsx
+++ components/exampleComponent.tsx
@@ -2,3 +2,11 @@
 
 // components folder stores all your reusable UI code
 // e.g. you built a killer navbar and you wanna reuse that all over your app on every page. put that component in this folder
+
+import React from "react";
+
+const exampleComponent = () => {
+  return <div>exampleComponent</div>;
+};
+
+export default exampleComponent;
