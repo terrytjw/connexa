29c45e3bac61dbd0197ba7872117a4d087399344
Terry Tan <tanjunweiterry@gmail.com>
2022-09-13 15:45:59 +0800

    Styling updates
    - home page layout and styling
    - global font family

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Button from "../components/Button";
 
 const HomePage = () => {
   return (
-    <div className="bg-slate-200">
+    <div className="bg-[#faf5f8]">
       <Head>
         <title>Crypto Connect</title>
         <meta
@@ -15,16 +15,22 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="mt-16 h-screen">
-        <h1 className="text-3xl font-serif font-bold pt-20 text-center">
-          Welcome to Crypto Connect!
-        </h1>
-        <Button
-          className="m-auto mt-6"
-          onClick={() => toast.success("Hooray!")}
-        >
-          Trigger Toast
-        </Button>
+      <div className="mt-16 flex justify-between h-screen">
+        <div className="grow-[2]">Left side bar</div>
+
+        <div className="border border-x-gray-300 grow-[5.5]">
+          <h1 className="text-3xl font-bold pt-20 text-center">
+            Welcome to Crypto Connect!
+          </h1>
+          <Button
+            className="m-auto mt-6"
+            onClick={() => toast.success("Hooray!")}
+          >
+            Trigger Toast
+          </Button>
+        </div>
+
+        <div className="grow-[2.5]">Right side bar</div>
       </div>
 
       <footer>
