6c2918946746dbf7fe527ccecb624be59fec3123
Harmon Chew <harmontchew@gmail.com>
2022-09-16 10:35:58 +0800

    dumb mistake

---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import Posts from "../../components/Posts";
+
+const PostPage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Posts />
+    </div>
+  );
+};
+
+export default PostPage();
