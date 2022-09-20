63990f419b75bb18ab87a1771e6d0f13c5a90f73
Harmon Chew <harmontchew@gmail.com>
2022-09-20 17:09:39 +0800

    fix build bug

--- components/QuizComponent.tsx
+++ components/QuizComponent.tsx
@@ -5,7 +5,9 @@ import { ArrowSmallRightIcon } from "@heroicons/react/24/solid";
 const QuizComponent = () => {
   const [slide, setSlide] = useState(0);
   const [score, setScore] = useState(0);
-  const [selectedOption, setSelectedOption] = useState<number | null>(null);
+  const [selectedOption, setSelectedOption] = useState<number | undefined>(
+    undefined
+  );
 
   const quiz = quizData[0];
 
@@ -14,22 +16,19 @@ const QuizComponent = () => {
       setScore((score) => score + 1);
     }
 
-    isCorrect !== null &&
-      console.log(quiz.quizQuestions[slide - 1].answers[selectedOption]);
     if (slide === 4) return;
 
     if (slide !== 0 && slide !== 3) {
-      if (selectedOption === null) return;
+      if (selectedOption === undefined) return;
     }
 
     setSlide(slide + 1);
-    setSelectedOption(null);
+    setSelectedOption(undefined);
   };
 
   const handleSelected = (id: number) => {
     setSelectedOption(id);
   };
-  console.log("selectedOption", selectedOption);
 
   return (
     <div className="border rounded-2xl border-black w-96 h-96 flex relative overflow-hidden ">
@@ -38,10 +37,10 @@ const QuizComponent = () => {
           className="absolute bottom-8 right-8 cursor-pointer hover:-translate-y-px w-8 h-8 z-10"
           onClick={() =>
             handleClick(
-              selectedOption !== null
+              selectedOption !== undefined
                 ? quiz.quizQuestions[slide - 1].answers[selectedOption]
                     .isCorrect
-                : null
+                : undefined
             )
           }
         >
@@ -59,12 +58,13 @@ const QuizComponent = () => {
         </div>
         {quiz.quizQuestions.map((question) => {
           return (
-            <div className="p-5 flex flex-col w-96 h-96 ">
+            <div className="p-5 flex flex-col w-96 h-96 " key={question.qID}>
               <div className="font-black text-xl text-center mb-3">Quiz</div>
               <div className="font-bold mb-5">{question.question}</div>
               {question.answers.map((answer) => {
                 return (
                   <div
+                    key={answer.id}
                     className={`w-full border-2 rounded-full p-2 mb-3 cursor-pointer pl-5 bg-slate-200  ${
                       selectedOption === answer.id
                         ? "border-2 border-black"
