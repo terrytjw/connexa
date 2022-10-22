861ed0b964bd3f67ee9fc873883cc00ca9a9d688
Terry Tan <tanjunweiterry@gmail.com>
2022-10-22 23:04:55 +0800

    updates

--- components/NewPrices.tsx
+++ components/NewPrices.tsx
@@ -3,45 +3,36 @@ import { collection, getFirestore, orderBy, query } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { auth } from "../lib/firebase";
 
-
 type Crypto = {
-    Token: string;
-    Price: string;
-    Change: string;
-    slug: string
-  };
-  
+  Token: string;
+  Price: string;
+  Change: string;
+  slug: string;
+};
+
 type Props = {
-    cryptocurrencies: Crypto[];
-  };
+  cryptocurrencies: Crypto[];
+};
 
 const CryptoFeed = ({ cryptocurrencies }: Props) => {
-    return (
-      <div className="">
-        {cryptocurrencies.map((crypto: Crypto ) => {
-          return <Crypto {...crypto} key={crypto.slug} />;
-        })}
-      </div>
-    );
-  };
-const Crypto = ({
-    Token, 
-    Price, 
-    Change, 
-    slug,
-  }: Crypto) => {
-    const [loading, setLoading] = useState(false);
-  
-  
-    return (
-      <div className="p-2 md:p-4">
-    
-    <h3 className=" mb-1 text-m leading-tight sm:leading-normal">
-    {Token}, {Price}, {Change}
-    </h3>
-  </div>
-  
-    );
-  };
-  
+  return (
+    <div>
+      {cryptocurrencies.map((crypto: Crypto) => {
+        return <Crypto {...crypto} key={crypto.slug} />;
+      })}
+    </div>
+  );
+};
+const Crypto = ({ Token, Price, Change, slug }: Crypto) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="p-2 md:p-4">
+      <h3 className=" mb-1 text-m leading-tight sm:leading-normal">
+        {Token}, {Price}, {Change}
+      </h3>
+    </div>
+  );
+};
+
 export default CryptoFeed;
