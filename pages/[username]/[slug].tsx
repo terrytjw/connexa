46c64dbe76584a49d8e70822c0bd6980116a9b54
Harmon Chew <harmontchew@gmail.com>
2022-11-06 16:50:20 +0800

    midst of single page

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,8 +1,34 @@
 // the post page bound to a specific username
+import { getDoc, firestore, getUserWithUsername } from "../../lib/firebase";
+import { GetServerSideProps } from "next";
 import React from "react";
+import { getUserProfileData } from "../api/getUserProfile/[username]";
+import { User } from "../../types";
 
-const PostPage = () => {
+type PostPageProps = {
+  user: User;
+  posts: any;
+  comments: any;
+  userDoc: any;
+};
+const PostPage = ({ userDoc }: PostPageProps) => {
+  console.log(userDoc);
   return <div>PostPage</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query: urlQuery,
+}) => {
+  const { username, slug } = urlQuery;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // const postPath = "users/" + username + "/posts/" + slug;
+  // const postDoc = await getDoc(firestore, postPath);
+
+  return {
+    props: { userDoc },
+  };
 };
 
 export default PostPage;
