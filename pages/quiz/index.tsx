12571785c4334a3a8431e902ce2c71980a841dbd
Terry Tan <tanjunweiterry@gmail.com>
2022-11-06 14:22:10 +0800

    added ts override

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -7,6 +7,7 @@ const DailyQuiz = function () {
 
   return (
     <div className="flex flex-row min-h-screen justify-center items-center">
+      {/* @ts-ignore */}
       <Quiz quiz={quiz} onComplete={setQuizResult} disableSynopsis />
     </div>
   );
